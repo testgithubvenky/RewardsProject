90f2d42a0a16075db968fbbaa30d19c673dcc117
testgithubvenky <kondojuvenkatesh1998@gmail.com>
2025-07-03 16:15:55 +0530

    Fix for half point bug

--- RewardsProject/src/main/java/com/rewards/program/service/RewardService.java
+++ RewardsProject/src/main/java/com/rewards/program/service/RewardService.java
@@ -61,10 +61,10 @@ public class RewardService {
 	public static int calculatePoints(double amount) {
 		int points = 0;
 		if(amount > 100) {
-			points += (int)(amount-100)*2;
+			points += (int)Math.round((amount-100)*2);
 			points += 50;
 		}else if(amount > 50) {
-			points += (int)(amount-50);
+			points += (int)Math.round(amount-50);
 		}
 		return points;
 	}
